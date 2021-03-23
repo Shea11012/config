936ba8097ba28fd50785aea33c2537f8c21d58ba
shea <mxyinvoker@gmail.com>
2021-03-23 11:16:09 +0800

    add install bash

---
+++ install.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+home=$1
+if [ -z $home]; then
+	echo "pleas enter HOME dir";
+	return;
+fi
+
+
+echo "cp nvim config to $home/.config...";
+cp -r nvim $home/.config
+
+echo "download vim plugin manager...";
+if command -v nvim > /dev/null 2>&1; then
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+		       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+else
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+		    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+echo ".zshrc config to $home...";
+cp zshrc/.zshrc $home;
+
+echo "download zplug...";
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
