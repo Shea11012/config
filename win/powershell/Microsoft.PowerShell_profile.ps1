21432b748773941dbb679c02ddfc6dbf5247d9fe
shea <mxyinvoker@gmail.com>
2023-01-06 04:17:03 +0800

    terminal custom theme

--- win/powershell/Microsoft.PowerShell_profile.ps1
+++ win/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 $Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
 
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/atomic.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\18723\workspace\remote\config\win\terminal\mytheme.omp.json" | Invoke-Expression
 
 sfst-hook | Invoke-Expression
 
