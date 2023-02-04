ddcf70e0a94f8c2442ae050719b9a08849b94ed4
shea <mxyinvoker@gmail.com>
2023-02-04 22:18:54 +0800

    rm profile.ps1 proxy

--- win/powershell/Microsoft.PowerShell_profile.ps1
+++ win/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,3 @@
-$Env:http_proxy="http://127.0.0.1:7890";$Env:https_proxy="http://127.0.0.1:7890"
-
 oh-my-posh init pwsh --config "C:\Users\18723\workspace\remote\config\win\terminal\mytheme.omp.json" | Invoke-Expression
 
 sfst-hook | Invoke-Expression
