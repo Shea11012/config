ddc6e54919ae8a11df3b459038800c10b7735521
mxy <mxyinvoker@gmail.com>
2023-04-05 22:47:10 +0800

    add custom function

--- scripts/init.zsh
+++ scripts/init.zsh
@@ -2,3 +2,8 @@
 function paru_update() {
     paru -Qauq | fzf -m | xargs -ro paru -S
 }
+
+# run subconverter
+function run_subconverter() {
+    docker run -d --name sub -p 25500:25500 -v sub-data:/base tindy2013/subconverter:latest
+}
