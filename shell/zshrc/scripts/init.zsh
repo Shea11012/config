4641ce49dd1b6e360c2fa224538809aed341ab58
mxy <mxyinvoker@gmail.com>
2023-08-25 17:02:34 +0800

    remove don't need track file

--- shell/zshrc/scripts/init.zsh
+++ shell/zshrc/scripts/init.zsh
@@ -3,12 +3,6 @@ function paru_update() {
     paru -Qauq | fzf -m | xargs -ro paru -S
 }
 
-# run subconverter
-function run_subconverter() {
-    subExists=`docker container ls -af 'name=sub'`
-    if [[ $subExists != '' ]]; then
-        docker restart sub
-        return 0
-    fi
-    docker run -d --name sub -p 25500:25500 -v sub-data:/base tindy2013/subconverter:latest
+function mount_share() {
+    sudo mount --mkdir -t cifs //192.168.32.100/share /mnt/share -o username=smbuser,password=123,iocharset=utf8,uid=1000,gid=1000
 }
