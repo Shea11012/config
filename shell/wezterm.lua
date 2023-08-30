02b97dbf04a7df1e9dc1b0fdb4b1f1f80200b14b
shea <mxyinvoker@gmail.com>
2023-08-30 23:01:02 +0800

    add wezterm configuration

---
+++ shell/wezterm.lua
@@ -0,0 +1,120 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+local default_prog = {}
+local launch_menu = {}
+
+-- windows
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+    default_prog = { 'pwsh', '-nol' }
+    table.insert(launch_menu, {
+        label = 'pwsh',
+        args = { 'pwsh', '-NoLogo' },
+    })
+
+    table.insert(launch_menu, {
+        label = "arch-wsl",
+        args = { 'wsl', '-d', 'arch', '--cd', '~' }
+    })
+
+    table.insert(launch_menu, {
+        label = 'nushell',
+        cwd = 'D:/scoop/shims/nu.exe',
+        args = { 'nu' }
+    })
+end
+
+-- linux
+if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
+    default_prog = { 'zsh', '-l' }
+end
+
+-- startup
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
+
+-- title
+local function basename(s)
+    return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    local pane = tab.active_pane
+    local index = ""
+    if #tabs > 1 then
+        index = string.format("%d: ", tab.tab_index + 1)
+    end
+
+    local process = basename(pane.foreground_process_name)
+
+    return { {
+        Text = ' ' .. index .. process .. ' '
+    } }
+end)
+
+local keys = {
+    { key = 'c',          mods = 'LEADER', action = act { SpawnTab = 'CurrentPaneDomain' } },
+    { key = 'x',          mods = 'LEADER', action = act { CloseCurrentPane = { confirm = true } } },
+    { key = 'X',          mods = 'LEADER', action = act { CloseCurrentTab = { confirm = true } } },
+    { key = 'l',          mods = 'ALT',    action = act.ShowLauncher },
+    -- pane
+    { key = 'LeftArrow',  mods = 'ALT',    action = act { ActivatePaneDirection = 'Next' } },
+    { key = 'RightArrow', mods = 'ALT',    action = act { ActivatePaneDirection = 'Prev' } },
+    -- { key = 'UpArrow',    mods = 'ALT',    action = act { ActivatePaneDirection = 'Up' } },
+    -- { key = 'DownArrow',  mods = 'ALT',    action = act { ActivatePaneDirection = 'Down' } },
+
+    -- tab
+    { key = 'LeftArrow',  mods = 'SHIFT',  action = act.ActivateTabRelative(-1) },
+    { key = 'RightArrow', mods = 'SHIFT',  action = act.ActivateTabRelative(1) },
+
+    -- split
+    { key = '-',          mods = 'WIN',    action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = '\\',         mods = 'WIN',    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+
+}
+
+local config = {
+    -- basic
+    enable_scroll_bar = true,
+    launch_menu = launch_menu,
+    color_scheme = 'One Dark (Gogh)',
+    default_prog = default_prog,
+
+    -- window
+    adjust_window_size_when_changing_font_size = true,
+    window_background_opacity = 0.9,
+    window_padding = {
+        left = 5,
+        right = 5,
+        top = 5,
+        bottom = 5,
+    },
+
+    inactive_pane_hsb = {
+        hue = 1.0,
+        saturation = 1.0,
+        brightness = 1.0
+    },
+
+    -- font
+    font = wezterm.font_with_fallback { 'JetBrainsMono NF', 'FiraCode Nerd Font' },
+    font_size = 16,
+    -- freetype_load_target = "Mono",
+
+    -- Tab bar
+    tab_bar_at_bottom = true,
+    tab_max_width = 25,
+
+    -- keys
+    leader = {
+        key = 'b',
+        mods = 'CTRL',
+    },
+    disable_default_key_bindings = false,
+    use_dead_keys = false,
+    keys = keys,
+}
+
+return config
--- shell/zshrc/README.md
+++ shell/zshrc/README.md
@@ -9,16 +9,8 @@ curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | 
 2. 安装必要软件
 
 ```bash
-paru -S fzf beautysh shellcheck lua-format unzip starship
-
 # 安装golang的版本管理工具
 curl -sSL https://git.io/g-install | sh -s -- zsh
-
-# 安装node版本管理工具
-curl -L https://bit.ly/n-install | bash
-
-# node安装prettier
-npm i -g prettier
 
 # 安装rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -26,6 +18,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 2.1 cli tools
 
+- paru instead of pacman and yay
 - uutils-coreutils insteadof coreutils
 - bat instead of cat
 - lsd instead of ls
@@ -36,7 +29,21 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 - zoxide instead of cd
 - bottom instead of top
 - ouch compression and decompression tool
+- fnm node version manager
+- just instead of make
+- dog instead of dig
+- jql instead of jq
+- wezterm cross-platform terminal write by rust 
 
 ```shell
-paru -S uutils-coreutils bat lsd fd hyperfine ripgrep tealdeer zoxide bottom ouch
+paru -S uutils-coreutils bat lsd fd hyperfine ripgrep tealdeer zoxide bottom ouch fnm-bin
+paru -S dog jql just fzf starship wezterm
+```
+
+2.2 language tool
+```bash
+paru -S beautysh shellcheck lua-format
+
+# node安装prettier
+npm i -g prettier
 ```
