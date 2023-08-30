6273ab9f56ee067b0d75d70af8685bf980ddde64
mxy <mxyinvoker@gmail.com>
2023-08-31 01:38:04 +0800

    update wezterm.lua

--- shell/wezterm.lua
+++ shell/wezterm.lua
@@ -3,30 +3,29 @@ local act = wezterm.action
 
 local default_prog = {}
 local launch_menu = {}
+local xim_im_name = ''
 
 -- windows
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    default_prog = { 'pwsh', '-nol' }
-    table.insert(launch_menu, {
-        label = 'pwsh',
-        args = { 'pwsh', '-NoLogo' },
-    })
+    default_prog = {'pwsh', '-nol'}
+    table.insert(launch_menu, {label = 'pwsh', args = {'pwsh', '-NoLogo'}})
 
     table.insert(launch_menu, {
         label = "arch-wsl",
-        args = { 'wsl', '-d', 'arch', '--cd', '~' }
+        args = {'wsl', '-d', 'arch', '--cd', '~'}
     })
 
     table.insert(launch_menu, {
         label = 'nushell',
         cwd = 'D:/scoop/shims/nu.exe',
-        args = { 'nu' }
+        args = {'nu'}
     })
 end
 
 -- linux
 if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
     default_prog = { 'zsh', '-l' }
+    xim_im_name = 'fcitx5'
 end
 
 -- startup
@@ -36,42 +35,57 @@ wezterm.on('gui-startup', function(cmd)
 end)
 
 -- title
-local function basename(s)
-    return string.gsub(s, '(.*[/\\])(.*)', '%2')
-end
+local function basename(s) return string.gsub(s, '(.*[/\\])(.*)', '%2') end
 
-wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+wezterm.on('format-tab-title',
+           function(tab, tabs, panes, config, hover, max_width)
     local pane = tab.active_pane
     local index = ""
-    if #tabs > 1 then
-        index = string.format("%d: ", tab.tab_index + 1)
-    end
+    if #tabs > 1 then index = string.format("%d: ", tab.tab_index + 1) end
 
     local process = basename(pane.foreground_process_name)
 
-    return { {
-        Text = ' ' .. index .. process .. ' '
-    } }
+    return {{Text = ' ' .. index .. process .. ' '}}
 end)
 
 local keys = {
-    { key = 'c',          mods = 'LEADER', action = act { SpawnTab = 'CurrentPaneDomain' } },
-    { key = 'x',          mods = 'LEADER', action = act { CloseCurrentPane = { confirm = true } } },
-    { key = 'X',          mods = 'LEADER', action = act { CloseCurrentTab = { confirm = true } } },
-    { key = 'l',          mods = 'ALT',    action = act.ShowLauncher },
-    -- pane
-    { key = 'LeftArrow',  mods = 'ALT',    action = act { ActivatePaneDirection = 'Next' } },
-    { key = 'RightArrow', mods = 'ALT',    action = act { ActivatePaneDirection = 'Prev' } },
+    {key = 'c', mods = 'LEADER', action = act {SpawnTab = 'CurrentPaneDomain'}},
+    {
+        key = 'x',
+        mods = 'LEADER',
+        action = act {CloseCurrentPane = {confirm = true}}
+    },
+    {
+        key = 'X',
+        mods = 'LEADER',
+        action = act {CloseCurrentTab = {confirm = true}}
+    }, {key = 'l', mods = 'ALT', action = act.ShowLauncher}, -- pane
+    {
+        key = 'LeftArrow',
+        mods = 'ALT',
+        action = act {ActivatePaneDirection = 'Next'}
+    }, {
+        key = 'RightArrow',
+        mods = 'ALT',
+        action = act {ActivatePaneDirection = 'Prev'}
+    },
     -- { key = 'UpArrow',    mods = 'ALT',    action = act { ActivatePaneDirection = 'Up' } },
     -- { key = 'DownArrow',  mods = 'ALT',    action = act { ActivatePaneDirection = 'Down' } },
 
     -- tab
-    { key = 'LeftArrow',  mods = 'SHIFT',  action = act.ActivateTabRelative(-1) },
-    { key = 'RightArrow', mods = 'SHIFT',  action = act.ActivateTabRelative(1) },
+    {key = 'LeftArrow', mods = 'SHIFT', action = act.ActivateTabRelative(-1)},
+    {key = 'RightArrow', mods = 'SHIFT', action = act.ActivateTabRelative(1)},
 
     -- split
-    { key = '-',          mods = 'WIN',    action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-    { key = '\\',         mods = 'WIN',    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    {
+        key = '-',
+        mods = 'WIN',
+        action = act.SplitVertical {domain = 'CurrentPaneDomain'}
+    }, {
+        key = '\\',
+        mods = 'WIN',
+        action = act.SplitHorizontal {domain = 'CurrentPaneDomain'}
+    }
 
 }
 
@@ -81,40 +95,31 @@ local config = {
     launch_menu = launch_menu,
     color_scheme = 'One Dark (Gogh)',
     default_prog = default_prog,
+    use_ime = true,
+    xim_im_name = xim_im_name,
 
     -- window
     adjust_window_size_when_changing_font_size = true,
     window_background_opacity = 0.9,
-    window_padding = {
-        left = 5,
-        right = 5,
-        top = 5,
-        bottom = 5,
-    },
+    window_padding = {left = 5, right = 5, top = 5, bottom = 5},
 
-    inactive_pane_hsb = {
-        hue = 1.0,
-        saturation = 1.0,
-        brightness = 1.0
-    },
+    inactive_pane_hsb = {hue = 1.0, saturation = 1.0, brightness = 1.0},
 
     -- font
-    font = wezterm.font_with_fallback { 'JetBrainsMono NF', 'FiraCode Nerd Font' },
+    font = wezterm.font_with_fallback { 'JetBrainsMono NF', 'FiraCode Nerd Font', 'Monospace' },
     font_size = 16,
     -- freetype_load_target = "Mono",
+    warn_about_missing_glyphs=false,
 
     -- Tab bar
     tab_bar_at_bottom = true,
     tab_max_width = 25,
 
     -- keys
-    leader = {
-        key = 'b',
-        mods = 'CTRL',
-    },
+    leader = {key = 'b', mods = 'CTRL'},
     disable_default_key_bindings = false,
     use_dead_keys = false,
-    keys = keys,
+    keys = keys
 }
 
 return config
