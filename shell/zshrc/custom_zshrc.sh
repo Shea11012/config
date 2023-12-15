0e66ee071facd3d9db27f032488deb54ec17e6d3
mxy <mxyinvoker@gmail.com>
2023-12-16 03:44:21 +0800

    add nerdctl & kubeadm completion

--- shell/zshrc/custom_zshrc.sh
+++ shell/zshrc/custom_zshrc.sh
@@ -1,9 +1,8 @@
-# Environment
 export EDITOR="nvim"
 export FZF_DEFAULT_OPTS="--inline-info --height 60% --ansi --border"
 # rootless docker daemon
-if command -v rg &> /dev/null && ! uname -a | rg -i "wsl" > /dev/null; then
-    export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+if command -v rg &>/dev/null && ! uname -a | rg -i "wsl" >/dev/null; then
+	export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
 fi
 
 #pyenv
@@ -20,6 +19,16 @@ export RUSTUP_UPDATE_ROOT="https://rsproxy.cn/rustup"
 
 # go
 export PATH="$HOME/go/bin/:$PATH"
+
+# nerdctl completion
+if command -v nerdctl &>/dev/null; then
+	source <(nerdctl completion zsh)
+fi
+
+# kubeadm completion
+if command -v kubeadm &>/dev/null; then
+	source <(kubeadm completion zsh)
+fi
 
 # alias
 alias vim="nvim"
